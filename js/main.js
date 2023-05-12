e9b59d582e8bd6432b127d807cd8c483a9d95e92
Odd Egil Hegge Selnes <post@odde.org>
2023-05-12 16:52:36 +0200

    Great improvement
    60% of pages complete

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ console.log('hei verden');
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navbarHeader = document.querySelector(".sticky-header");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    navbarHeader.classList.toggle("active");
 });
